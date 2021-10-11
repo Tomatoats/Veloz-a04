242564fb8ea5fcc133f62faaff9596e47c669979
Alexys Veloz <alexys.veloz.fl@gmail.com>
2021-10-10 22:37:19 -0400

    This is the arrayList version of it

--- exercise41/src/main/java/baseline/StringSorter.java
+++ exercise41/src/main/java/baseline/StringSorter.java
@@ -10,44 +10,34 @@ import java.util.Collections;
 public class StringSorter {
 
 
-    private void PrintToTxt(String[] Stringnames, ArrayList names) throws FileNotFoundException {
+    private void PrintToTxt(ArrayList SortedNames) throws FileNotFoundException {
         //PrintStream
         PrintStream out = new PrintStream(new FileOutputStream("exercise41_output.txt"));
         System.setOut(out);
 
         //Print from element 0 to the last element, the sorted names
-        int size = names.size();
+        int size = SortedNames.size();
         System.out.printf("There are a total of %d names:%n--------------------------%n",size);
         for (int i = 0; i < size; i++)
         {
-            System.out.printf("%s", Stringnames[i]);
+            System.out.println(SortedNames.get(i));
         }
     }
 
-    private String[] Sort(String[] Stringnames) {
+    private ArrayList Sort(ArrayList names) {
         //Use arrays.Sort() to sort the string and return it
-        Arrays.sort(Stringnames);
-        return Stringnames;
+        Collections.sort(names);
+        return names;
     }
     public StringSorter(ArrayList names) throws FileNotFoundException {
         //hey so weird things are happening imma try to make this a string first in a new function
-        String[] Stringnames = Stringify(names);
 
         //bring it to a function to sort the string [] / arrayList
 
-        String[] SortedNames = Sort(Stringnames);
+        ArrayList SortedNames = Sort(names);
 
         //bring it to a function to print to a txt
-        PrintToTxt(Stringnames, names);
+        PrintToTxt(SortedNames);
     }
 
-    private String[] Stringify(ArrayList names) {
-        int size = names.size();
-        String[] StringName = new String[size];
-        for (int i = 0; i < size; i++)
-        {
-            StringName[i] = (String) names.get(i);
-        }
-        return StringName;
-    }
 }
