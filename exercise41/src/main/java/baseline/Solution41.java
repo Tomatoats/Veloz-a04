42531183fcb80e5623b8cb51773e1346b6127a6b
Alexys Veloz <alexys.veloz.fl@gmail.com>
2021-10-10 21:43:59 -0400

    Code mostly done: this is the String[] version

--- exercise41/src/main/java/baseline/Solution41.java
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -1,12 +1,20 @@
 package baseline;
-
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Paths;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Solution41 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         /*
          *  UCF COP3330 Fall 2021 Assignment 4 Solutions 41
          *  Copyright 2021 Alexys Veloz
          */
+
         /* Okay so we're gonna take in the input from the text file, using
         a delimiter to collect a new string probably unless that doesn't work, and
         once we have the names in a string array, send it to a new class with two methods:
@@ -14,14 +22,25 @@ public class Solution41 {
         and the print function will take it and print it to an output.txt
          */
 
+        //Arraylist seemed easier so we're doing  an arrayList instead
+        ArrayList<String> names = new ArrayList<String>();
         //take in the  input.txt, using the try, do, and catch to make sure everything works
-        //also use a while to make sure it continues after the delimiter
+        try (Scanner input = new Scanner(Paths.get("exercise41_input.txt")).useDelimiter("\n"))
+        {
+            //also use a while to make sure it continues after the delimiter
 
-        //put it into a String[]
-        //if String[] doesn't work, then an arrayList instead
-        String[] names = new String[0];
-        //Send it to another class
-
-        StringSorter userString = new StringSorter(names);
-    }
-}
+            while (input.hasNext())
+            { //while there is more to read
+                //put it into a arrayList
+                names.add(input.next());
+            }
+        }
+            //put in the catch
+        catch (IOException | NoSuchElementException | IllegalStateException e)
+        {
+            e.printStackTrace();
+            //Send it to another class
+        }
+                StringSorter userString = new StringSorter(names);
+            }
+        }
--- exercise41/src/main/java/baseline/StringSorter.java
+++ exercise41/src/main/java/baseline/StringSorter.java
@@ -1,23 +1,53 @@
 package baseline;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class StringSorter {
 
 
-    private void PrintToTxt(String[] sortedNames) {
+    private void PrintToTxt(String[] Stringnames, ArrayList names) throws FileNotFoundException {
         //PrintStream
+        PrintStream out = new PrintStream(new FileOutputStream("exercise41_output.txt"));
+        System.setOut(out);
+
         //Print from element 0 to the last element, the sorted names
+        int size = names.size();
+        System.out.printf("There are a total of %d names:%n--------------------------%n",size);
+        for (int i = 0; i < size; i++)
+        {
+            System.out.printf("%s", Stringnames[i]);
+        }
     }
 
-    private String[] Sort(String[] names) {
+    private String[] Sort(String[] Stringnames) {
         //Use arrays.Sort() to sort the string and return it
-        return null;
+        Arrays.sort(Stringnames);
+        return Stringnames;
     }
-    public StringSorter(String[] names) {
+    public StringSorter(ArrayList names) throws FileNotFoundException {
+        //hey so weird things are happening imma try to make this a string first in a new function
+        String[] Stringnames = Stringify(names);
 
         //bring it to a function to sort the string [] / arrayList
-        String [] SortedNames = Sort(names);
+
+        String[] SortedNames = Sort(Stringnames);
 
         //bring it to a function to print to a txt
-        PrintToTxt(SortedNames);
+        PrintToTxt(Stringnames, names);
+    }
+
+    private String[] Stringify(ArrayList names) {
+        int size = names.size();
+        String[] StringName = new String[size];
+        for (int i = 0; i < size; i++)
+        {
+            StringName[i] = (String) names.get(i);
+        }
+        return StringName;
     }
 }
